6e89416a0b1ed7433ba1a8acecee7f20dc9e8852
minivalley01 <nppot.winas@gmail.com>
2026-03-02 10:49:52 +0700

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 
-
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('test')
-  test(): string[]{
-     return this.userService.test();
-    }
+  test(): string[] {
+    return this.userService.test();
+  }
 }
